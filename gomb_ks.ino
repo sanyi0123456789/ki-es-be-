433c3a759de97abbae5d1a2321a977a5a3b88e07
sanyi0123456789 <githubiskola@gmail.com>
2025-04-04 10:17:07 +0200

    Add files via upload

---
+++ gomb_ks.ino
@@ -0,0 +1,31 @@
+// Pinok beállítása
+const int buttonPin = 2;     // A gomb pinje
+
+// Változók
+int buttonState = 0;         // A gomb állapota
+int lastButtonState = 0;     // Az előző gomb állapota
+
+void setup() {
+  // Gomb pin beállítása bemeneti módra
+  pinMode(buttonPin, INPUT);
+  
+  // Soros kommunikáció inicializálása
+  Serial.begin(9600);
+}
+
+void loop() {
+  // A gomb állapotának olvasása
+  buttonState = digitalRead(buttonPin);
+  
+  // Ha a gomb állapota változik (lefelé vagy felfelé)
+  if (buttonState != lastButtonState) {
+    // Ha a gomb meg lett nyomva (állapot változott LOW-ra)
+    if (buttonState == HIGH) {
+      Serial.println("A gomb meg lett nyomva!");
+    } else {
+      Serial.println("A gomb el van engedve!");
+    }
+    // Frissítjük az előző állapotot
+    lastButtonState = buttonState;
+  }
+}
